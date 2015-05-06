655fddc62c954dd2447895d06c1be224363b25ec
Kevin R. Thornton <krthornt@uci.edu>
2015-05-06 06:33:31 -0700

    fixed dereference (issue #2 again)

--- src/g2.c
+++ src/g2.c
@@ -15,7 +15,7 @@ void g2(double *x, size_t size, double *G, double *correction, double *pG,double
   double sumLnFreq = a*log(a)+b*log(b)+c*log(c)+d*log(d);
   double sumLnTots = (a+b)*log(a+b) + (c+d)*log(c+d) + (a+c)*log(a+c) + (b+d)*log(b+d);
   *G = 2.*(sumLnFreq - sumLnTots + n*log(n));/* the G statistic itself */
-  if(G*<0)*G=0.;
+  if(*G<0)*G=0.;
   /* correction = William's correction */
   *correction =1.+( ((n/(a+b))+(n/(c+d))-1.)*((n/(a+c))+(n/(b+d))-1.))/(6*n);
   *pG = gsl_sf_gamma_inc_Q(0.5,0.5*(*G));/* probability is chi-squared with df = 1 */
